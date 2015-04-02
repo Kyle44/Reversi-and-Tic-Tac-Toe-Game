931e5a708364b711a9a9ada4e1050bb54120fc04
Kyle44 <kyle44@comcast.net>
2015-04-02 01:46:11 -0400

    Create Proj4Aux.h

---
+++ Proj4Aux.h
@@ -0,0 +1,13 @@
+// File: Proj4Aux.h
+
+#ifndef PROJ4AUX_H
+#define PROJ4AUX_H
+
+#include "GridGame.h"
+
+GridGame *SetupGame(int argc, char *argv[]);
+bool GameSpecificIsDone(GridGame *game);
+void DoOnePlayer(GridGame *game, int player);
+void GameSpecificOutputResults(GridGame *game);
+
+#endif //PROJ4AUX_H
